a34c3c2f87f57061000d37a25b3cf6fc0e69c9fa
trharris-cyber <tr.harris@aol.com>
2026-05-01 22:21:43 -0500

    Update command deployment to global scope

--- kaze-clan-discord-bot/index.js
+++ kaze-clan-discord-bot/index.js
@@ -57,14 +57,10 @@ const commands = [
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 async function deployCommands() {
-  await rest.put(
-    Routes.applicationGuildCommands(
-      process.env.CLIENT_ID,
-      process.env.GUILD_ID
-    ),
-    { body: commands }
-  );
-
+ await rest.put(
+  Routes.applicationCommands(process.env.CLIENT_ID),
+  { body: commands }
+);
   console.log("Slash commands deployed.");
 }
 
